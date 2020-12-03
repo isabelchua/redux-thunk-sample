8af0f60d31e4b0bbde7e06af711a67abea7a2c8e
isabelchua <isabelchua86@gmail.com>
2020-12-03 12:42:43 -0800

    setup

--- src/index.js
+++ src/index.js
@@ -4,17 +4,39 @@ import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import App from "./App";
 import { counterReducer } from "./reducer";
+import logger from "redux-logger";
 
-const myLogger = store => {
-	return next => {
-		return action => {
-			console.log("middleware ran");
-			return next(action);
-		};
-	};
+const myLogger = store => next => action => {
+	//console.log("middleware ran");
+	return next(action);
 };
 
-const store = createStore(counterReducer, applyMiddleware(myLogger));
+const secondMiddleware = store => next => action => {
+	//console.log("Second middleware ran");
+	return next(action);
+};
+
+const capAtTen = store => next => action => {
+	//console.log(store);
+	if (store.getState() >= 10) {
+		return next({ type: "DECREMENT" });
+	}
+	next(action);
+};
+
+// const myLogger = store => {
+// 	return next => {
+// 		return action => {
+// 			console.log("middleware ran");
+// 			return next(action);
+// 		};
+// 	};
+// };
+
+const store = createStore(
+	counterReducer,
+	applyMiddleware(myLogger, secondMiddleware, capAtTen, logger)
+);
 
 ReactDOM.render(
 	<Provider store={store}>
